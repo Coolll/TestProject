7d60a33615e73d30a279778b62dea54521cf79d6
龙培 <longpei@longpeideMacBook-Pro.local>
2017-07-11 16:28:55 +0800

    change file

--- TestProject/ViewController.m
+++ TestProject/ViewController.m
@@ -25,6 +25,21 @@
 }
 
 
+- (void)testOneMethod
+{
+    NSLog(@"方法1");
+}
+
+- (void)testTwoMethod
+{
+    NSLog(@"写在branch dev2.1中");
+    NSLog(@"我在dev2.1中又增加了一个打印");
+}
+
+- (void)testFourMethod
+{
+    NSLog(@"这个是在dev2.1中的");
+}
 
 
 - (void)didReceiveMemoryWarning {
